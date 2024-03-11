f9dababb39a5f45e86e522b32613a59a5f8f471f
JethroMuluba <jethro.muluba@gmail.com>
2024-03-11 14:17:47 +0100

    try to post

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// });
+
+app.post('/', (req, res) => {
+    res.send('<h1>Got a POST request</h1>');
 });
+
 
 app.listen(port, () => {
     console.log(`Example app listerning on port ${port}`)
-})
+});
+console.log('Erreur lors de la création du serveur');
